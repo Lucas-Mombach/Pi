ee4384cd1a6ff61fa9aa44f2a3a750517a454997
Lucas Mombach <lucasmmombach@gmail.com>
2021-09-12 21:48:41 -0300

    Comentarios adicionados ao arquivo Wallis.cpp

--- Wallis.cpp
+++ Wallis.cpp
@@ -12,25 +12,26 @@ int main(){
   deno= 1;
   cont1 = 1;
   cont2 = 1;
+  
 
   cin >> termos;
-
+  /* os denominadores dos termos muda a cada dois termos com excessão do primeiro que é 1*/
   while (termos >= cont1)
   {
-    if (cont2 == 2){
-      
+    if (cont2 == 2){ //cont2 é o contador dos que uso pra definir os dois termos com o mesmo denominador
+  
       deno += 2;
-      num = deno-1;
-      cont2=0;
+      num = deno-1; //sempre que for o segundo termo a ter aquele denominador
+      cont2=0; // vai ser somado +1 no final no loop
     }
     else
     {
-      if (cont1==1)
+      if (cont1==1) // só vai rodar logo ao entrar no loop
       {
         num = 2;
       }
 
-      else
+      else // vai rodar sempre que o termo for o primeiro a ter aquele denominador
       {
         num = deno+1;
         }
